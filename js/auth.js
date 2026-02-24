e41b96cb212d81ee6a79d4dde8dcdaf72052356c
tamimbhaihere321 <tamimbhaihere321@gmail.com>
2026-02-24 20:09:28 +0600

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,25 @@
+import {
+createUserWithEmailAndPassword,
+signInWithPopup
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { auth, googleProvider } from "./firebase.js";
+
+export async function signup(email,password){
+  try{
+    await createUserWithEmailAndPassword(auth,email,password);
+    alert("Signup successful");
+    window.location.href="dashboard.html";
+  }catch(err){
+    alert(err.message);
+  }
+}
+
+export async function googleLogin(){
+  try{
+    await signInWithPopup(auth,googleProvider);
+    window.location.href="dashboard.html";
+  }catch(err){
+    alert(err.message);
+  }
+}
